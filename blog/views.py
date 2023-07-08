8697a33596c1a8858a9f67e9a3e8f09761597be0
baiv84 <65267292+baiv84@users.noreply.github.com>
2023-07-08 13:27:39 +0700

    Add base template layout support

--- blog/views.py
+++ blog/views.py
@@ -22,15 +22,13 @@ def serialize_post(post):
 
 
 def index(request):
-    """
-    Вьюхи не оптимизированы, потому что в последней задаче модуля Django ORM нужно их оптимизировать как раз на примере этого сайта.
-    """
-    
+    """Main page representation view"""
     all_posts = Post.objects.prefetch_related('author')
     popular_posts = all_posts.annotate(likes_count=Count('likes')).order_by('-likes_count')[:3]
     fresh_posts = all_posts.order_by('-published_at')[:5]
 
     context = {
+        "page_title": "Main page",
         "most_popular_posts": [serialize_post(post) for post in popular_posts],
         "fresh_posts": [serialize_post(post) for post in fresh_posts],
     }
@@ -39,9 +37,7 @@ def index(request):
 
 
 def post_detail(request, slug):
-    """
-    Вьюхи не оптимизированы, потому что в последней задаче модуля Django ORM нужно их оптимизировать как раз на примере этого сайта.
-    """
+    """Post page representation view"""
     post = get_object_or_404(Post, slug=slug)
     comments = Comment.objects.filter(post=post)
     serialized_comments = []
@@ -64,17 +60,14 @@ def post_detail(request, slug):
     }
 
     context = {
+        "page_title": "Post page",
         "post": serialized_post,
     }
     return render(request, 'blog-details.html', context)
 
 
 def contact(request):
-    """
-    Вьюхи не оптимизированы, потому что в последней задаче модуля Django ORM нужно их оптимизировать как раз на примере этого сайта.
-    """
-    # позже здесь будет код для статистики заходов на эту страницу
-    # и для записи фидбека
+    """Contact page representation view"""
     folium_map = folium.Map(location=COMPANY_COORDINATES, zoom_start=12)
     folium.Marker(
         COMPANY_COORDINATES,
